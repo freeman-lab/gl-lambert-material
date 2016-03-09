f001f6dc302a0404334ebd78fbaae5ab38ccc82a
freeman <the.freeman.lab@gmail.com>
2016-03-09 18:41:36 -0500

    pluralize

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var glslify = require('glslify')
 
 module.exports = {
   fragment: glslify('./fragment.glsl'),
-  style: {
+  styles: {
     emissive: {type: 'vec3', default: [0.0, 0.0, 0.0]},
     ambient: {type: 'vec3', default: [0.2, 0.2, 0.2]},
     diffuse: {type: 'vec3', default: [0.8, 0.8, 0.8]},
