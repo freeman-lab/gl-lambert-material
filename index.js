884dd81ab9e355108efd28171dc5a67b3162d21f
freeman <the.freeman.lab@gmail.com>
2016-03-09 17:59:18 -0500

    use standard

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var glslify = require('glslify')
 module.exports = {
   fragment: glslify('./fragment.glsl'),
   style: {
-    emissive: {type: 'vec3', default: [0.0, 0.0, 0.0]}, 
-    ambient: {type: 'vec3', default: [0.2, 0.2, 0.2]}, 
-    diffuse: {type: 'vec3', default: [0.8, 0.8, 0.8]}, 
+    emissive: {type: 'vec3', default: [0.0, 0.0, 0.0]},
+    ambient: {type: 'vec3', default: [0.2, 0.2, 0.2]},
+    diffuse: {type: 'vec3', default: [0.8, 0.8, 0.8]},
     roughness: {type: 'float', default: 0.7, min: 0, max: 1},
     albedo: {type: 'float', default: 0.7, min: 0, max: 1}
   },
