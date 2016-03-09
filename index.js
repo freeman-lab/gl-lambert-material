ba2f2a44e7dc4a9132e2608e89169e369a933c3f
freeman <the.freeman.lab@gmail.com>
2016-03-09 17:55:19 -0500

    add name

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var glslify = require('glslify')
 
 module.exports = {
   fragment: glslify('./fragment.glsl'),
-  uniforms: {
+  style: {
     emissive: {type: 'vec3', default: [0.0, 0.0, 0.0]}, 
     ambient: {type: 'vec3', default: [0.2, 0.2, 0.2]}, 
     diffuse: {type: 'vec3', default: [0.8, 0.8, 0.8]}, 
     roughness: {type: 'float', default: 0.7, min: 0, max: 1},
     albedo: {type: 'float', default: 0.7, min: 0, max: 1}
   },
+  name: 'lambert'
 }
